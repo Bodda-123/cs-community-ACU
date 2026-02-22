864e3eb292bd2c6c328153ffbeafeec1681d6e14
Abdulrahman Abo alsouad <abdelrahmanaboalsouad@gmail.com>
2026-02-22 23:05:01 +0200

    Enable debug mode to diagnose Internal Server Error

--- index.py
+++ index.py
@@ -29,4 +29,5 @@ app = application
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # نستخدم debug=True مؤقتاً لمعرفة سبب الـ Internal Server Error
+    app.run(host="0.0.0.0", port=port, debug=True)
